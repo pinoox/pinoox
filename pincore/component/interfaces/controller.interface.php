bd093d28e2bd05de79c6b480964d961688598079
yoosef alipour <yoosefalipour@gmail.com>
2019-10-18 17:11:02 +0330

    add PHPDoc to controller interface

--- pincore/component/interfaces/controller.interface.php
+++ pincore/component/interfaces/controller.interface.php
@@ -14,7 +14,21 @@ namespace pinoox\component\interfaces;
 
 interface ControllerInterface
 {
+    /**
+     * Main method
+     *
+     * if not a parameter
+     *
+     * @return mixed
+     */
     public function _main();
 
+    /**
+     * Exception method
+     *
+     * if it is a parameter and not a method for it
+     *
+     * @return mixed
+     */
     public function _exception();
 }
