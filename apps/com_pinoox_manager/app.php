d5dde3e9045183f8ba78c14cf8c5859ab7e3109c
yoosef alipour <yoosefalipour@gmail.com>
2019-12-28 18:37:31 +0330

    Add background in manager

--- apps/com_pinoox_manager/app.php
+++ apps/com_pinoox_manager/app.php
@@ -1,24 +1,22 @@
 <?php
-//pinoox app file, generated at "2019-12-28 10:57"
+//pinoox app file, generated at "2019-12-28 14:38"
 
-return array (
-  'enable' => true,
-  'router' => false,
-  'theme' => 'spark',
-  'name' => 'تنظیمات',
-  'description' => 'Applications Management System (AMS)',
-  'icon' => 'icon.png',
-  'version-name' => '1.3',
-  'version-code' => 2,
-  'developer' => 'Pinoox Team',
-  'minpin' => 1,
-  'open' => 'setting-dashboard',
-  'sys-app' => true,
-  'service' => 
-  array (
-    0 => 'cache/update',
-  ),
-  'lang' => 'fa',
+return array(
+    'enable' => true,
+    'router' => false,
+    'theme' => 'spark',
+    'name' => 'تنظیمات',
+    'description' => 'Applications Management System (AMS)',
+    'icon' => 'icon.png',
+    'version-name' => '1.3',
+    'version-code' => 2,
+    'developer' => 'Pinoox Team',
+    'minpin' => 1,
+    'open' => 'setting-dashboard',
+    'sys-app' => true,
+    'service' => [
+        'cache/update'
+    ]
 );
 
 //end of app
--- apps/com_pinoox_manager/config/options.config.php
+++ apps/com_pinoox_manager/config/options.config.php
@@ -1,21 +1,9 @@
 <?php
-//pinoox config file, generated at "2019-12-28 12:59"
+//pinoox config file, generated at "2019-12-28 14:58"
 
 return array (
-  'background' => '5',
+  'background' => '7',
   'lock_time' => 0,
-  'pinoox_auth' => 
-  array (
-    'avatar' => 'https://www.pinoox.com/apps/com_pinoox_hub/uploads/avatar/15772031335233.jpg',
-    'avatar_thumb' => 'https://www.pinoox.com/apps/com_pinoox_hub/uploads/avatar/thumbs/15772031335233_128.jpg',
-    'user_id' => 100,
-    'fullname' => NULL,
-    'email' => 'info@pinoox.com',
-    'username' => 'pinoox',
-    'nickname' => 'پینوکس',
-    'isLogin' => true,
-    'token' => '88d4122434e07a0d89f6d1b6e3b90ad3',
-  ),
 );
 
 //end of config
