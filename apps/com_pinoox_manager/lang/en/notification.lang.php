57411291f862d484689f74e354d37dd98a99b784
yoosef alipour <yoosefalipour@gmail.com>
2020-08-28 18:07:47 +0430

    Add notification for check version & update new version

---
+++ apps/com_pinoox_manager/lang/en/notification.lang.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'release_new_version' => [
+        'title' => 'Release new version',
+        'message' => 'The new version of Pinoox {version} has been released',
+    ],
+    'update_new_version' => [
+        'title' => 'Update',
+        'message' => 'The new version of Pinoox {version} has been successfully updated',
+    ],
+];
