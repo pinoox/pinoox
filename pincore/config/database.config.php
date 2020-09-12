54cde014b7b17be67d0a68d382c30f99276b4954
esmaeil bahrani fard <esmaeil94@gmail.com>
2020-09-13 00:54:42 +0430

    Auto stash before merge of "manager" and "develop"

---
+++ pincore/config/database.config.php
@@ -0,0 +1,12 @@
+<?php
+//pinoox config file, generated at "2020-09-04 20:52"
+
+return array (
+  'host' => 'localhost',
+  'database' => 'pinoox',
+  'username' => 'root',
+  'password' => NULL,
+  'prefix' => NULL,
+);
+
+//end of config
