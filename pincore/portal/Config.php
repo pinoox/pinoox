06955aeaf6372bd98d09323f2ef9123237ec1415
Esmaeil Bahrani Fard <44259840+esmaeilbahrani@users.noreply.github.com>
2023-09-11 12:09:49 +0330

    Update Config.php

--- pincore/portal/Config.php
+++ pincore/portal/Config.php
@@ -14,105 +14,73 @@
 
 namespace pinoox\portal;
 
-use pinoox\portal\app\App;
 use pinoox\component\package\reference\ReferenceInterface;
 use pinoox\component\source\Portal;
-use pinoox\component\store\Config as ObjectPortal1;
+use pinoox\component\store\config\Config as ObjectPortal1;
+use pinoox\component\store\config\data\DataArray;
+use pinoox\component\store\config\strategy\FileConfigStrategy;
 
 /**
- * @method static \pinoox\component\store\Config create(\pinoox\component\store\Pinker $pinker, mixed $merge = NULL)
- * @method static \pinoox\component\store\Config ___()
+ * @method static Config save()
+ * @method static mixed get(?string $key = NULL)
+ * @method static ObjectPortal1 add(string $key, mixed $value)
+ * @method static ObjectPortal1 set(string $key, mixed $value)
+ * @method static ObjectPortal1 remove(string $key)
+ * @method static ObjectPortal1 merge(array $array)
+ * @method static ObjectPortal1 reset()
+ * @method static ObjectPortal1 restore()
+ * @method static setLinear(string $key, string $target, mixed $value)
+ * @method static getLinear(string $key, string $target)
+ * @method static \pinoox\component\store\config\Config  object()
  *
- * @see \pinoox\component\store\Config
+ * @see \pinoox\component\store\config\Config
  */
 class Config extends Portal
 {
-	const folder = 'config';
+    const folder = 'config';
 
-	public static function __register(): void
-	{
-		self::__bind(ObjectPortal1::class)->setArguments([Pinker::__ref()]);
-	}
+    public static function __register(): void
+    {
+        self::__bind(ObjectPortal1::class)->setArguments([]);
+    }
 
+    /**
+     * Get the registered name of the component.
+     * @return string
+     */
+    public static function __name(): string
+    {
+        return 'config';
+    }
 
-	/**
-	 * Set file for pinoox baker
-	 *
-	 * @param string|ReferenceInterface $fileName
-	 * @return ObjectPortal1
-	 */
-	public static function name(string|ReferenceInterface $fileName): ObjectPortal1
-	{
-		$fileName = $fileName . '.config.php';
-		$reference = Path::prefixReference($fileName, self::folder);
-		return Config::create(Pinker::file($reference));
-	}
+    /**
+     * Set file for pinoox baker
+     *
+     * @param string|ReferenceInterface $fileName
+     * @return ObjectPortal1
+     */
+    public static function name(string|ReferenceInterface $fileName): ObjectPortal1
+    {
+        return self::initFileConfig($fileName);
+    }
 
+    private static function initFileConfig(string $fileName): ObjectPortal1
+    {
+        $fileName = $fileName . '.config.php';
+        $ref = Path::prefixReference($fileName, self::folder);
+        $pinker = Pinker::file($ref);
+        $array = new DataArray($pinker->pickup());
+        return new ObjectPortal1(new FileConfigStrategy($pinker, $array));
+    }
 
-	/**
-	 * Set file for pinoox baker
-	 *
-	 * @param string $name
-	 */
-	private function initData(string $name): void
-	{
-		$this->name = $name;
-		$this->app = null;
-		$parts = explode(':', $name);
-		if (count($parts) === 2) {
-		    $this->app = $parts[0];
-		    $name = $parts[1];
-		}
-
-		$name = str_replace(['/', '\\'], '>', $name);
-		$filename = $name;
-		if (HelperString::firstHas($name, '~')) {
-		    $filename = HelperString::firstDelete($filename, '~');
-		    $appDefault = '~';
-		} else {
-		    $appDefault = App::package();
-		}
-
-		$this->app = !empty($app) ? $app : $appDefault;
-
-		$file = 'config/' . $filename . '.config.php';
-		$file = ($this->app === '~') ? '~' . $file : $file;
-		$this->pinker->file($file);
-
-		$this->key = $this->app . ':' . $filename;
-		if (!isset(self::$data[$this->key])) {
-		    $value = $this->pinker->pickup();
-		    self::$data[$this->key] = $value;
-		}
-	}
-
-
-	/**
-	 * Get the registered name of the component.
-	 * @return string
-	 */
-	public static function __name(): string
-	{
-		return 'config';
-	}
-
-
-	/**
-	 * Get include method names .
-	 * @return string[]
-	 */
-	public static function __include(): array
-	{
-		return ['name','create'];
-	}
-
-
-	/**
-	 * Get method names for callback object.
-	 * @return string[]
-	 */
-	public static function __callback(): array
-	{
-		return [];
-	}
+    /**
+     * Get method names for callback object.
+     * @return string[]
+     */
+    public static function __callback(): array
+    {
+        return [
+            'save'
+        ];
+    }
 }
