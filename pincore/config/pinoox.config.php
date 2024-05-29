ac77b897dea6032f6eb97f9c154759dcdc3fe104
yoosef alipour <yoosefalipour@gmail.com>
2024-05-29 21:32:40 +0330

    Update pinoox.config.php

--- pincore/config/pinoox.config.php
+++ pincore/config/pinoox.config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'version_code' => 17,
-    'version_name' => '2.0.0',
+    'version_code' => 18,
+    'version_name' => '2.0.1',
     'apps_location' => 'apps',
     'lang' => 'en',
     'mode' => 'development', //development | production
