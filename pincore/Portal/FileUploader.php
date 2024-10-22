39320888feb8f8d227a8ea7eec6160ed4fa5c6c5
Esmaeil Bahrani Fard <44259840+esmaeilbahrani@users.noreply.github.com>
2024-10-22 15:04:40 +0330

    Update FileUploader.php

--- pincore/Portal/FileUploader.php
+++ pincore/Portal/FileUploader.php
@@ -33,81 +33,80 @@ use Pinoox\Portal\App\App;
  */
 class FileUploader extends Portal
 {
-    public static function __register(): void
-    {
-        self::__bind(ObjectPortal1::class)
-            ->addMethodCall('addEvent', [
-                Upload\Event::Insert,
-                function (ObjectPortal2 $uploader) {
-                    self::insertFileModel($uploader);
-                }
-            ])->addMethodCall('addEvent', [
-                Upload\Event::Delete,
-                function (ObjectPortal2 $uploader) {
-                    self::deleteFileModel($uploader);
-                }
-            ]);
-    }
+	public static function __register(): void
+	{
+		self::__bind(ObjectPortal1::class)
+		    ->addMethodCall('addEvent', [
+		        Upload\Event::Insert,
+		        function (ObjectPortal2 $uploader) {
+		            self::insertFileModel($uploader);
+		        }
+		    ])->addMethodCall('addEvent', [
+		        Upload\Event::Delete,
+		        function (ObjectPortal2 $uploader) {
+		            self::deleteFileModel($uploader);
+		        }
+		    ]);
+	}
 
 
-    private static function insertFileModel(ObjectPortal2 $uploader): void
-    {
- 
-        $model = FileModel::create([
-            'user_id' => User::get('user_id'),
-            'app' => App::package(),
-            'file_name' => $uploader->getFilename(),
-            'file_realname' => $uploader->getFileRealname(),
-            'file_ext' => $uploader->getExtension(),
-            'file_path' => $uploader->getDestination(),
-            'file_size' => $uploader->getSize(),
-            'file_access' => $uploader->getAccess(),
-            'hash_id' => $uploader->getHashId(),
-            'file_metadata' => $uploader->getMetaData(),
-            'file_group' => $uploader->getGroup(),
-        ]);
- 
-        if ($model) {
-            $uploader->setResult('file_id', $model->file_id);
-        }
-    }
+	private static function insertFileModel(ObjectPortal2 $uploader): void
+	{
+		$model = FileModel::create([
+		    'user_id' => User::get('user_id'),
+		    'app' => App::package(),
+		    'file_name' => $uploader->getFilename(),
+		    'file_realname' => $uploader->getFileRealname(),
+		    'file_ext' => $uploader->getExtension(),
+		    'file_path' => $uploader->getDestination(),
+		    'file_size' => $uploader->getSize(),
+		    'file_access' => $uploader->getAccess(),
+		    'hash_id' => $uploader->getHashId(),
+		    'file_metadata' => $uploader->getMetaData(),
+		    'file_group' => $uploader->getGroup(),
+		]);
+
+		if ($model) {
+		    $uploader->setResult('file_id', $model->file_id);
+		}
+	}
 
 
-    private static function deleteFileModel(ObjectPortal2 $uploader): void
-    {
-        FileModel::where('file_id', $uploader->getFileModel()->file_id)->delete();
-    }
+	private static function deleteFileModel(ObjectPortal2 $uploader): void
+	{
+		FileModel::where('file_id', $uploader->getFileModel()->file_id)->delete();
+	}
 
 
-    /**
-     * Get the registered name of the component.
-     * @return string
-     */
-    public static function __name(): string
-    {
-        return 'file.uploader';
-    }
+	/**
+	 * Get the registered name of the component.
+	 * @return string
+	 */
+	public static function __name(): string
+	{
+		return 'file.uploader';
+	}
 
 
-    /**
-     * Get exclude method names .
-     * @return string[]
-     */
-    public static function __exclude(): array
-    {
-        return [];
-    }
+	/**
+	 * Get exclude method names .
+	 * @return string[]
+	 */
+	public static function __exclude(): array
+	{
+		return [];
+	}
 
 
-    /**
-     * Get method names for callback object.
-     * @return string[]
-     */
-    public static function __callback(): array
-    {
-        return [
-            'setDestination',
-            'setFile'
-        ];
-    }
+	/**
+	 * Get method names for callback object.
+	 * @return string[]
+	 */
+	public static function __callback(): array
+	{
+		return [
+		    'setDestination',
+		    'setFile'
+		];
+	}
 }
