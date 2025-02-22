76614e93b5e51e95bf1bcf29e54d02f6e9263f44
Esmaeil Bahrani Fard <44259840+esmaeilbahrani@users.noreply.github.com>
2025-02-22 09:01:19 +0330

    Update AddEditModal to show real domain

--- apps/com_pinoox_manager/theme/spark/src/views/pages/control/routes/routes-home.vue
+++ apps/com_pinoox_manager/theme/spark/src/views/pages/control/routes/routes-home.vue
@@ -1,56 +1,57 @@
 <template>
-    <Page title="مسیریابی" class="pageRoutes">
-        <template #toolbar>
-            <Menu @click="openModalAddEditRoute()" :icon="saxIcon.add" label="افزودن"/>
-            <Menu @click="openModal(ModalGuide, { message: guideMessage })" :icon="saxIcon.guide" label="راهنما"/>
-        </template>
+  <Page title="مسیریابی" class="pageRoutes">
+    <template #toolbar>
+      <Menu @click="openModalAddEditRoute()" :icon="saxIcon.add" label="افزودن"/>
+      <Menu @click="openModal(ModalGuide, { message: guideMessage })" :icon="saxIcon.guide" label="راهنما"/>
+    </template>
 
-        <div v-if="routeStore.routeList.length" class="overflow-x-auto">
-            <table class="min-w-full divide-y divide-gray-800">
-                <thead class="">
-                <tr>
-                    <th class="px-6 py-3 text-right text-xs font-medium text-gray-200 uppercase tracking-wider">
-                        اپلیکیشن
-                    </th>
-                    <th class="px-6 py-3 text-right text-xs font-medium text-gray-200 uppercase tracking-wider">آدرس
-                        مسیر
-                    </th>
-                    <th class="px-6 py-3 text-right text-xs font-medium text-gray-200 uppercase tracking-wider">عملیات
-                    </th>
-                </tr>
-                </thead>
-                <tbody class=" divide-gray-200">
-                <tr v-for="(route,index) in routeStore.routeList" :key="index">
-                    <td class="px-4 whitespace-nowrap text-sm text-gray-200">
-                        <div class="flex items-center">
-                            <img :src="app(route.package).icon" :alt="app(route.package).name" class="w-8 h-8 mr-2"/>
-                            <span class="pr-4">{{ app(route.package).name }}</span>
-                        </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300 ltr">
-                        {{ currentSite }}/{{ route.path }}
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
-                        <button @click="editRoute(route)" v-if="!route.is_lock" class=" hover:text-blue-300 ml-4">
-                            <Icon :is="saxIcon.edit"></Icon>
-                        </button>
-                        <button @click="deleteRoute(route.path)" v-if="!route.is_lock && route.path !== '/'"
-                                class="hover:text-red-700 ml-4">
-                            <Icon :is="saxIcon.remove"></Icon>
-                        </button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
+    <div v-if="routeStore.routeList.length" class="overflow-x-auto">
+      <table class="min-w-full divide-y divide-gray-800">
+        <thead class="">
+        <tr>
+          <th class="px-6 py-3 text-right text-xs font-medium text-gray-200 uppercase tracking-wider">
+            اپلیکیشن
+          </th>
+          <th class="px-6 py-3 text-right text-xs font-medium text-gray-200 uppercase tracking-wider">آدرس
+            مسیر
+          </th>
+          <th class="px-6 py-3 text-right text-xs font-medium text-gray-200 uppercase tracking-wider">عملیات
+          </th>
+        </tr>
+        </thead>
+        <tbody class=" divide-gray-200">
+        <tr v-for="(route,index) in routeStore.routeList" :key="index">
+          <td class="px-4 whitespace-nowrap text-sm text-gray-200">
+            <div class="flex items-center">
+              <img :src="app(route.package).icon" :alt="app(route.package).name" class="w-8 h-8 mr-2"/>
+              <span class="pr-4">{{ app(route.package).name }}</span>
+            </div>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap">
+            <a class="ltr  text-sm text-gray-300" target="_blank" :href="currentSite+'/'+route.path">{{ currentSite + '/' + route.path }}</a>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+            <Button variant="secondary" @click="editRoute(route)" v-if="!route.is_lock"
+                    class=" hover:text-blue-300 ml-4">
+              <Icon :is="saxIcon.edit"></Icon>
+            </Button>
+            <Button variant="secondary" @click="deleteRoute(route.path)" v-if="!route.is_lock && route.path !== '/'"
+                    class="hover:text-red-700 ml-4">
+              <Icon :is="saxIcon.remove"></Icon>
+            </Button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
 
-        <PageEmpty
-                v-else
-                title="هیچ مسیری ثبت نشده است"
-                description="برای افزودن مسیر جدید، روی دکمه افزودن کلیک کنید."
-                :icon="saxIcon.routes"
-        />
-    </Page>
+    <PageEmpty
+        v-else
+        title="هیچ مسیری ثبت نشده است"
+        description="برای افزودن مسیر جدید، روی دکمه افزودن کلیک کنید."
+        :icon="saxIcon.routes"
+    />
+  </Page>
 </template>
 
 <script setup>
@@ -80,30 +81,29 @@ const guideMessage = ref(
 );
 
 function openModalAddEditRoute(route = null) {
-    openModal(ModalAddEditRoute, {payload: route}).then((res) => {
-        console.log(payload, res);
-    });
+  openModal(ModalAddEditRoute, {payload: route}).then((res) => {
+  });
 }
 
 function editRoute(route) {
-    openModalAddEditRoute(route);
+  openModalAddEditRoute(route);
 }
 
 function deleteRoute(path) {
-    routerAPI.remove({
-        path: path,
-    }).then(() => {
-        routeStore.deleteRouteByPath(path);
-    })
+  routerAPI.remove({
+    path: path,
+  }).then(() => {
+    routeStore.deleteRouteByPath(path);
+  })
 
 }
 
 const add = () => {
-    routerAPI.add()
+  routerAPI.add()
 }
 
 const app = (packageName) => {
-    return appStore.fetchAppByPackage(packageName);
+  return appStore.fetchAppByPackage(packageName);
 }
 
 </script>
