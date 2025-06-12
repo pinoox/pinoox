5397744d8de0103788f064cf030eeb08bdb7b7cc
Esmaeil Bahrani Fard <44259840+esmaeilbahrani@users.noreply.github.com>
2025-06-12 12:16:19 +0330

    Update pinoox.config.php

--- pincore/config/pinoox.config.php
+++ pincore/config/pinoox.config.php
@@ -5,13 +5,7 @@ return [
     'version_name' => '2.1.0',
     'apps_location' => 'apps',
     'lang' => 'en',
-    'mode' => 'test', //development | production | test
-    'cache' => [
-        'driver'             => 'symfony',     // symfony | pinker
-        'directory'          => sys_get_temp_dir(),
-        'default_namespace'  => 'app_cache_',
-        'default_lifetime'   => 3600,          // seconds
-    ],
+    'mode' => 'development', //development | production | test
     'log' => [
         'path' => sys_get_temp_dir() . '/pinoox.log',
         'channel' => 'app',
