230d6d4e15aabd7fa33d01e4377fd993733e830a
yoosefap <yoosefalipour@gmail.com>
2026-06-09 04:11:03 +0330

    refactor: streamline installer lang and prerequisites flow

---
+++ apps/com_pinoox_installer/theme/magic/src/lang/en/user.js
@@ -0,0 +1,9 @@
+export default {
+    info_admin: 'Site admin account',
+    info_admin_description: 'Create an account to manage your site.',
+    name: 'First name',
+    family_name: 'Last name',
+    email: 'Email',
+    username: 'Username',
+    password: 'Password',
+}
---
+++ apps/com_pinoox_installer/theme/magic/src/lang/fa/user.js
@@ -0,0 +1,9 @@
+export default {
+    info_admin: 'حساب مدیر سایت',
+    info_admin_description: 'یک حساب کاربری برای مدیریت سایت بسازید.',
+    name: 'نام',
+    family_name: 'نام خانوادگی',
+    email: 'ایمیل',
+    username: 'نام کاربری',
+    password: 'رمز عبور',
+}
