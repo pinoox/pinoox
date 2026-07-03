130f91f8b246d229e5913b94750446943df47c40
Esmaeil Bahrani Fard <44259840+esmaeilbahrani@users.noreply.github.com>
2026-07-03 21:09:12 +0330

    Remove com_pinoox_comingsoon bundled app

--- apps/com_pinoox_comingsoon/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-use App\com_pinoox_comingsoon\Controller\MainController;
-use function Pinoox\Router\{get, post};
-get('/', [MainController::class, 'main'])->name('home');
-get('/panel', [MainController::class, 'panel'])->name('panel');
-post('/panel', [MainController::class, 'save'])->name('panel.save');
-get('*', fn () => redirect(url('/')))->name('fallback');
-
