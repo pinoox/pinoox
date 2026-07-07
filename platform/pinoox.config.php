d218a6f7ed3f0d69ef8ead633f67bbfe47540afd
yoosefap <yoosefalipour@gmail.com>
2026-07-07 10:52:41 +0330

    fix: prefer local pincore classmap over vendor in CLI boot

--- platform/pinoox.config.php
+++ platform/pinoox.config.php
@@ -11,6 +11,6 @@ return [
     | Kernel version: pincore/config/pincore.config.php
     |
     */
-    'version_code' => 42,
-    'version_name' => '3.3.1',
+    'version_code' => 43,
+    'version_name' => '3.3.2',
 ];
